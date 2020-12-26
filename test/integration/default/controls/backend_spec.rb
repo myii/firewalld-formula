e19c533ca0046806c40acbe197d78c96faa2e025
Imran Iqbal <iqbalmy@hotmail.com>
2020-12-26 20:07:44 +0000

    ci: test `map.jinja` verification across unique platforms

--- test/integration/default/controls/backend_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-describe package('nftables') do
-  it { should be_installed }
-end
--- test/integration/default/controls/ipset_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-describe package('ipset') do
-  it { should be_installed }
-end
